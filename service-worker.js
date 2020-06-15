4547a3524dda5b668f3bb6832b0d0e94155ab088
arrjayy <rajan.aggarwal@ten10.com>
2020-06-15 23:41:48 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/objectiveyes-webapp/precache-manifest.498d15abcfdaeeb11705c95cbb8b0e27.js"
+  "/objectiveyes-webapp/precache-manifest.beae99e1bb6ce6521a2861a18add3e7a.js"
 );
 
 self.addEventListener('message', (event) => {
